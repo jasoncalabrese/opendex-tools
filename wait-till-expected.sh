da9341bbf6aa95e68fcb1bf65156e92955b91374
Jason Calabrese <jason@cbrese.com>
2016-06-12 17:45:45 -0700

    split out time-since.sh and added an is-fresh.sh script

--- wait-till-expected.sh
+++ wait-till-expected.sh
@@ -3,8 +3,7 @@
 GLUCOSE=$1
 
 OLD=${2-5}	
-TIME_SINCE=`cat /home/indy/cgm-loop/monitor/glucose-zoned.json | json -e "this.minAgo=(new Date()-new Date(this.dateString))/60/1000" | json -a minAgo | head -n 1`
-
+TIME_SINCE=$(time-since.sh $GLUCOSE)
 
 if (( $(bc <<< "$TIME_SINCE >= $OLD") )); then
   echo "CGM Data $TIME_SINCE mins ago is old (>=$OLD), not waiting"
@@ -12,5 +11,6 @@ else
   WAIT_MINS=$(bc <<< "$OLD - $TIME_SINCE")
   echo "CGM Data $TIME_SINCE mins ago is fresh (< $OLD), waiting $WAIT_MINS mins for new data"
   time sleep ${WAIT_MINS}m
+  echo "finished waiting, lets get some CGM Data"
 fi
 
